8820e797e4c630dd966bfb25f7e70382bfb6f728
enzo <enzo.delispostipro@gmail.com>
2026-01-23 16:15:04 +0100

    ajoutarg

--- shell_42/test/main.c
+++ shell_42/test/main.c
@@ -1,6 +1,6 @@
 #include "token.h"
 
-struct token *get_last_token(struct token* tokens)
+struct token *get_last_token(struct token* tokens) //recupere le dernier token
 {
     struct token *element;
 
@@ -14,7 +14,7 @@ struct token *get_last_token(struct token* tokens)
     return(element);
 }
 
-struct token *new_token(struct token* tokens, char *value, char* type)
+struct token *new_token(struct token* tokens, char *value, char* type) //cree le nouveau token
 {
     struct token *next;
     struct token *last_element;
@@ -121,47 +121,44 @@ char *find_type(char *str, struct data *data) // cherche les type
 	int i;
 
 	i = 0;
-	type = malloc(sizeof(char) * 10);
-	if (!type)
-		return (NULL);
-	else if (ft_strncmp(str, "echo", 5) == 0 || ft_strncmp(str, "cd", 2) == 0 || ft_strncmp(str, "pwd", 3) == 0  || ft_strncmp(str, "export", 7) == 0  || ft_strncmp(str, "unset", 6) == 0  || ft_strncmp(str, "env", 4) == 0  || ft_strncmp(str, "exit", 5) == 0 )
+	if (ft_strncmp(str, "echo", 5) == 0 || ft_strncmp(str, "cd", 2) == 0 || ft_strncmp(str, "pwd", 3) == 0  || ft_strncmp(str, "export", 7) == 0  || ft_strncmp(str, "unset", 6) == 0  || ft_strncmp(str, "env", 4) == 0  || ft_strncmp(str, "exit", 5) == 0 )
 	{
-		type = "commande";
+		type = ft_strdup("commande");
 		return (type);
 	}
 	else if (ft_strncmp(str, "|", 2) == 0)
 	{
-		type = "pipe";
+		type = ft_strdup("pipe");
 		return (type);
 	}
 	else if (ft_strncmp(str, ">>", 3) == 0)
 	{
-		type = "dgreat";
+		type = ft_strdup("dgrat");
 		return(type);
 	}
 	else if (ft_strncmp(str, ">", 2) == 0)
 	{
-		type = "great";
+		type = ft_strdup("great");
 		return(type);
 	}
 	else if (ft_strncmp(str, "<<", 3) == 0)
 	{
-		type = "dless";
+		ft_strdup("dless");
 		return(type);
 	}
 	else if (ft_strncmp(str, "<", 2) == 0)
 	{
-		type = "less";
+		type = ft_strdup("less");
 		return(type);
 	}
 	else if (check_acces(str, data) == 1)
 	{
-		type = "commande";
+		type = ft_strdup("commande");
 		return (type);
 	}
 	else
 	{
-		type = "mot";
+		type = ft_strdup("mot");
 		return (type);
 	}
 	return (NULL);
@@ -183,7 +180,7 @@ void destroy_tokens(struct token* tokens)
 	}
 }
 
-int free_all(char *line, struct data *data, struct token *tokens) // free
+void free_all(char *line, struct data *data, struct token *tokens) // free
 {
 	int i;
 
@@ -209,13 +206,20 @@ int free_all(char *line, struct data *data, struct token *tokens) // free
 
 	destroy_tokens(tokens);
 }
+
 int main(int ac, char **av, char **env)
 {
 	struct data *data;
 	struct token *tokens;
 	char *line;
 	int i;
+	char **arg;
+	char *tmp;
 
+	arg = malloc(sizeof(char *) * ac);
+	if (!arg)
+		return(0);
+	arg[ac] = NULL;
 	i = 0;
 	tokens = malloc(sizeof(struct token));
 	if (!tokens)
@@ -234,7 +238,35 @@ int main(int ac, char **av, char **env)
 		data->result_split = ft_split(line, ' ');
 		while (data->result_split[i])
 		{
-			tokens = new_token(tokens, data->result_split[i], find_type(data->result_split[i], data));
+			arg[i] = find_type(data->result_split[i], data);
+			if (ft_strncmp(arg[i], "commande", 9)  == 0 && data->result_split[i + 1] != NULL)
+			{
+				i++;
+				while (data->result_split[i])
+				{
+					if (tmp)
+						free(tmp);
+					tmp = find_type(data->result_split[i], data);
+					printf("%s\n", tmp);
+					if (ft_strncmp(tmp, "pipe", 5) == 0 || ft_strncmp(tmp, "dgreat", 7) == 0 || ft_strncmp(tmp, "great", 6) == 0 || ft_strncmp(tmp, "less", 5) == 0 || ft_strncmp(tmp, "dless", 5) == 0)
+					{
+						printf("OK");
+						arg[i] = find_type(data->result_split[i], data);
+						if (tmp)
+							free(tmp);
+						break;
+					}
+					arg[i] = ft_strdup("arg");
+					i++;
+				}
+			}
+			if (data->result_split[i] != NULL)
+				i++;
+		}
+		i = 0;
+		while (data->result_split[i])
+		{
+			tokens = new_token(tokens, data->result_split[i], arg[i]);
 			i++;
 		}
 	}
--- shell_42/test/token.h
+++ shell_42/test/token.h
@@ -1,14 +1,9 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-#include <stdlib.h>
 #include "libft/libft.h"
 #include "../src/lexer/lexer.h"
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "../src/tokens/token.h"
 
 struct token
 {
@@ -16,16 +11,5 @@ struct token
     char* type; // "> << >> < |" , '\n' , ou un mot , commande
     struct token *next; // pointe vers le prochain token (termine par NULL)
 };
-
-struct data
-{
-    char **result_split; //resultat de slpit de readline
-    char **env; //copie de l'environement
-    char **path_split; //chemin du path
-};
-
-struct token *new_token(struct token* tokens, char *value, char* type); //crée un nouveau token
-void free_token(struct token* tokens); // detruit un token
-void destroy_tokens(struct token* tokens); // detruit tout les token
 
 #endif
